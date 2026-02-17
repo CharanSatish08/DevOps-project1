8263eb5d17afb5b975346bf4d30f0d6bf5887b36
Charan <charansatish08@gmail.com>
2026-02-17 07:55:53 -0600

    correct the gateway id syntax

--- Terraform/Modules/main.tf
+++ Terraform/Modules/main.tf
@@ -22,7 +22,7 @@ resource "aws_route_table" "route_table" {
  vpc_id = aws_vpc.main.id
  route {
    cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.internet_gateway
+   gateway_id = aws_internet_gateway.internet_gateway.id
  }
  tags = {
    Name = "${var.project_name}-rt"
